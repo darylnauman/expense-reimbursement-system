96c5d18c240ade873b45e7cb560d4e74514efa5f
Daryl <darylnauman@gmail.com>
2022-05-01 20:50:04 -0400

    Add another RR constructor

--- request-reimbursement/src/main/java/com/ex/requestreimbursement/models/ReimbursementRequest.java
+++ request-reimbursement/src/main/java/com/ex/requestreimbursement/models/ReimbursementRequest.java
@@ -29,4 +29,11 @@ public class ReimbursementRequest {
 
     @Column(name = "manager_id")
     private int managerId;
+
+    public ReimbursementRequest(String item, float amount, int employeeId, int managerId) {
+        this.item = item;
+        this.amount = amount;
+        this.employeeId = employeeId;
+        this.managerId = managerId;
+    }
 }
